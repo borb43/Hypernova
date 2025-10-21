4574c0851a2182030199c7669569b178bf008881
Eris (borb) <remcosveen@gmail.com>
2025-10-21 17:55:37 +0200

    reorganization 1

--- items/misc_joker.lua
+++ items/misc_joker.lua
@@ -1,3 +1,20 @@
+SMODS.Joker { --fusion reactor, balances before scoring
+    key = "fusion",
+    atlas = "placeholder",
+    pos = { x = 1, y = 0 },
+    rarity = 2,
+    cost = 7,
+    blueprint_compat = true,
+    demicoloncompat = true,
+    calculate = function(self, card, context)
+        if context.initial_scoring_step or context.forcetrigger then
+            return {
+                balance = true
+            }
+        end
+    end
+}
+
 SMODS.Joker { --growth, increases potency of other joker effects
     key = "growth",
     atlas = "placeholder",
--- items/probability_joker.lua
+++ items/probability_joker.lua
@@ -1,15 +1,18 @@
-SMODS.Joker { --fusion reactor, balances before scoring
-    key = "fusion",
+SMODS.Joker { --tipping scales, increases numerator and denominator of probabilities
+    key = "scales",
     atlas = "placeholder",
-    pos = { x = 1, y = 0 },
-    rarity = 2,
-    cost = 7,
-    blueprint_compat = true,
-    demicoloncompat = true,
-    calculate = function(self, card, context)
-        if context.initial_scoring_step or context.forcetrigger then
+    pos = { x = 0, y = 0 },
+    config = { extra = { mod = 1 } },
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mod } }
+    end,
+    rarity = 1,
+    cost = 5,
+    calculate = function (self, card, context)
+        if context.mod_probability and not context.blueprint then
             return {
-                balance = true
+                numerator = context.numerator + card.ability.extra.mod,
+                denominator = context.denominator + card.ability.extra.mod
             }
         end
     end
