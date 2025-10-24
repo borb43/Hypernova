e244bb1ad192c82affe76691d7719ed67a7743e5
Eris (borb) <remcosveen@gmail.com>
2025-10-24 20:58:39 +0200

    no more talisman arrows manip :tada:

--- lib/manipulate.lua
+++ lib/manipulate.lua
@@ -4,14 +4,14 @@ HPR.manipulate_blacklist = {
     message_card = true,
     playing_card = true,
     end_of_round = true,
-    retriggers = true
+    retriggers = true,
+    [1] = true
 }
 
 HPR.janky_manipulates = {
     cards_to_draw = true,
     numerator = true,
     denominator = true,
-    arrows = true
 }
 
 HPR.manipulate_ret = function(table, mod) -- multiplies values in a table by `mod`. intended for joker returns in context.post_trigger
@@ -19,7 +19,7 @@ HPR.manipulate_ret = function(table, mod) -- multiplies values in a table by `mo
         if type(table[k]) == "number" and not HPR.manipulate_blacklist[k] and (HPR.config.enable_janky_manips or not HPR.janky_manipulates[k]) then
             if k == "level_up" and table[k] == true then table[k] = 1 end
             table[k] = table[k] * mod
-            if k == "dollars" or k == "p_dollars" or k == "h_dollars" or k == "level_up" or k == "arrows" then
+            if k == "dollars" or k == "p_dollars" or k == "h_dollars" or k == "level_up" then
                 table[k] = math.floor(table[k])
             end
         elseif type(v) == "table" and not v.area and not HPR.manipulate_blacklist[k] then
