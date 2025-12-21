38ce42baf13f2ecd0c0e6adda4627f74aeae63c0
Eris (borb) <remcosveen@gmail.com>
2025-12-21 20:15:43 +0100

    no more extra crossmod badge

--- crossmod/xmod_moons.lua
+++ crossmod/xmod_moons.lua
@@ -1,27 +1,27 @@
-HPR.moon {
-    key = "greip",
-    atlas = "placeholder",
-    dependencies = { "potassium_re" },
-    pos = { x = 3, y = 2 },
-    config = { extra = 0.1, max_highlighted = 2 },
-    loc_vars = function (self, info_queue, card)
-        return { vars = { card.ability.extra, card.ability.max_highlighted }}
-    end,
-    apply_bonus = function (self, card, other_card)
-        other_card.ability.kali_perma_glop = other_card.ability.kali_perma_glop + card.ability.extra
-    end
-}
+if next(SMODS.find_mod("potassium_re")) then
+    HPR.moon {
+        key = "greip",
+        atlas = "placeholder",
+        pos = { x = 3, y = 2 },
+        config = { extra = 0.1, max_highlighted = 2 },
+        loc_vars = function(self, info_queue, card)
+            return { vars = { card.ability.extra, card.ability.max_highlighted } }
+        end,
+        apply_bonus = function(self, card, other_card)
+            other_card.ability.kali_perma_glop = other_card.ability.kali_perma_glop + card.ability.extra
+        end
+    }
 
-HPR.moon {
-    key = "gerd",
-    atlas = "placeholder",
-    dependencies = { "potassium_re" },
-    pos = { x = 3, y = 2 },
-    config = { extra = 0.1, max_highlighted = 3 },
-    loc_vars = function (self, info_queue, card)
-        return { vars = { card.ability.extra, card.ability.max_highlighted }}
-    end,
-    apply_bonus = function (self, card, other_card)
-        other_card.ability.kali_perma_h_glop = other_card.ability.kali_perma_h_glop + card.ability.extra
-    end
-}
+    HPR.moon {
+        key = "gerd",
+        atlas = "placeholder",
+        pos = { x = 3, y = 2 },
+        config = { extra = 0.1, max_highlighted = 3 },
+        loc_vars = function(self, info_queue, card)
+            return { vars = { card.ability.extra, card.ability.max_highlighted } }
+        end,
+        apply_bonus = function(self, card, other_card)
+            other_card.ability.kali_perma_h_glop = other_card.ability.kali_perma_h_glop + card.ability.extra
+        end
+    }
+end
