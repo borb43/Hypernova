11ba77a08c0c2fb8984006ebbe86433621abef04
Eris (borb) <remcosveen@gmail.com>
2025-12-25 21:41:18 +0100

    oops wrong order

--- items/prophecy.lua
+++ items/prophecy.lua
@@ -47,6 +47,53 @@ SMODS.Consumable { --tome, reverse ectoplasm
 }
 
 --#region hidden consumables
+SMODS.Consumable { --ascension, reverse the soul
+    key = "ascender",
+    atlas = "placeholder",
+    pos = { x = 2, y = 2 },
+    --soul_pos,
+    set = "hpr_prophecy",
+    cost = 4,
+    hidden = true,
+    can_use = function (self, card)
+        return G.jokers and #G.jokers.highlighted == 1 and HPR.get_ascension(G.jokers.highlighted[1])
+    end,
+    use = function (self, card, area, copier)
+        G.E_MANAGER:add_event(Event{
+            trigger = 'after',
+            delay = 0.4,
+            func = function ()
+                play_sound('tarot1')
+                card:juice_up(0.3, 0.5)
+                return true
+            end
+        })
+        G.E_MANAGER:add_event(Event{
+            trigger = 'after',
+            delay = 0.15,
+            func = function ()
+                G.jokers.highlighted[1]:flip()
+                play_sound('card1')
+                return true
+            end
+        })
+        delay(0.2)
+        G.E_MANAGER:add_event(Event{
+            trigger = 'after',
+            delay = 0.15,
+            func = function ()
+                G.jokers.highlighted[1]:remove_from_deck()
+                G.jokers.highlighted[1]:set_ability(HPR.get_ascension(G.jokers.highlighted[1]))
+                G.jokers.highlighted[1]:add_to_deck()
+                G.jokers.highlighted[1]:set_cost()
+                G.jokers.highlighted[1]:flip()
+                return true
+            end
+        })
+        delay(0.5)
+    end
+}
+
 SMODS.Consumable { --pulsar, reverse black hole
     key = "pulsar",
     set = "hpr_prophecy",
@@ -126,52 +173,5 @@ SMODS.Consumable { --pulsar, reverse black hole
         idea = "Eris",
         code = "Eris"
     }
-}
-
-SMODS.Consumable { --ascension, reverse the soul
-    key = "ascender",
-    atlas = "placeholder",
-    pos = { x = 2, y = 2 },
-    --soul_pos,
-    set = "hpr_prophecy",
-    cost = 4,
-    hidden = true,
-    can_use = function (self, card)
-        return G.jokers and #G.jokers.highlighted == 1 and HPR.get_ascension(G.jokers.highlighted[1])
-    end,
-    use = function (self, card, area, copier)
-        G.E_MANAGER:add_event(Event{
-            trigger = 'after',
-            delay = 0.4,
-            func = function ()
-                play_sound('tarot1')
-                card:juice_up(0.3, 0.5)
-                return true
-            end
-        })
-        G.E_MANAGER:add_event(Event{
-            trigger = 'after',
-            delay = 0.15,
-            func = function ()
-                G.jokers.highlighted[1]:flip()
-                play_sound('card1')
-                return true
-            end
-        })
-        delay(0.2)
-        G.E_MANAGER:add_event(Event{
-            trigger = 'after',
-            delay = 0.15,
-            func = function ()
-                G.jokers.highlighted[1]:remove_from_deck()
-                G.jokers.highlighted[1]:set_ability(HPR.get_ascension(G.jokers.highlighted[1]))
-                G.jokers.highlighted[1]:add_to_deck()
-                G.jokers.highlighted[1]:set_cost()
-                G.jokers.highlighted[1]:flip()
-                return true
-            end
-        })
-        delay(0.5)
-    end
 }
 --#endregion
